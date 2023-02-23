7273e64c0a8cda6247eac4ec917da9e813d3b0e3
Nathan Brizendine <nathanb@spotify.com>
2023-02-23 14:47:10 +0100

    Start parsing bazel output

--- ruler-cli/src/main/java/com/spotify/ruler/cli/RulerCli.kt
+++ ruler-cli/src/main/java/com/spotify/ruler/cli/RulerCli.kt
@@ -6,8 +6,11 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
 import com.spotify.rulercommon.BaseRulerTask
+import com.spotify.rulercommon.dependency.ArtifactResult
+import com.spotify.rulercommon.dependency.DefaultArtifactParser
 import com.spotify.rulercommon.models.RulerConfig
 import com.spotify.rulercommon.dependency.DependencyComponent
+import com.spotify.rulercommon.dependency.JarArtifactParser
 import com.spotify.rulercommon.models.AppInfo
 import com.spotify.rulercommon.models.DeviceSpec
 import kotlinx.serialization.ExperimentalSerializationApi
@@ -51,14 +54,23 @@ class RulerCli: CliktCommand(), BaseRulerTask {
             )
         }
     override fun getDependencies(): Map<String, List<DependencyComponent>> {
-        TODO("Not yet implemented")
+        val json = Json.decodeFromStream<ModuleMap>(dependencyMap.inputStream())
+
+        val jarDepedencies = json.jars.flatMap {
+            JarArtifactParser().parseFile(ArtifactResult.JarArtifact(
+                File(it.jar), it.module
+            ))
+        }
+
+        var assets = json.assets.map {
+
+
+        }
     }
 
     override fun run() {
         super.run()
     }
-
-
 }
 
 @Serializable
@@ -72,6 +84,25 @@ data class JsonRulerConfig(
     val deviceSpec: DeviceSpec,
     val defaultOwner: String,
     val omitFileBreakdown: Boolean
+)
+
+@Serializable
+data class ModuleMap (
+    val assets: List<Asset>,
+    val jars: List<Jar>,
+    val resources: List<Asset>
+)
+
+@Serializable
+data class Asset (
+    val filename: String,
+    val module: String
+)
+
+@Serializable
+data class Jar (
+    val jar: String,
+    val module: String
 )
 
 fun main(args: Array<String>) = RulerCli().main(args)
